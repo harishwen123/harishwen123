763a44e221adf1265cf7a82d3ba5f77f9ff13b68
harishwen123 <135607810+harishwen123@users.noreply.github.com>
2023-06-06 12:53:46 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,20 @@
+Python 3.12.0b1 (tags/v3.12.0b1:5612078, May 22 2023, 16:20:31) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\User\OneDrive\Documents\my first porgram.py
+Hello, World!
+what is your name?harishwen
+Hello harishwen
+>>> age=12
+>>> print(age)
+12
+>>> weight=35
+>>> print(weight)
+35
+>>> height=149cm
+SyntaxError: invalid decimal literal
+>>> height=141cm
+SyntaxError: invalid decimal literal
+>>> height=67
+>>> print(height)
+67
